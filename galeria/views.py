ffcb16e1ad485a75bb2e0fe43592e2e0dabd7ce5
Bruno Divino <brunodivino.dev@gmail.com>
2022-12-01 17:57:39 -0300

    Resultado após vídeo 1.3

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'galeria/index.html')
+
+    dados = {
+        1: {"nome":"Nebulosa de Carina",
+            "legenda":"webbtelescope.org / NASA / James Webb"},
+        2: {"nome":"Galáxia NGC 1079",
+            "legenda":"nasa.org / NASA / Hubble"}
+    }
+
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
