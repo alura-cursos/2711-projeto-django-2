858019b49d5eb9f3e1def41956e40fb1a429d6f9
Bruno Divino <brunodivino.dev@gmail.com>
2022-12-01 22:27:39 -0300

    Resultado após vídeo 2.4

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from galeria.models import Fotografia
+
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda")
+    list_display_links = ("id","nome")
+    search_fields = ("nome",)
+
+admin.site.register(Fotografia, ListandoFotografias)
