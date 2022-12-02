1498bb70ecd48a20d444b13b72bb320d72882cbe
Bruno Divino <brunodivino.dev@gmail.com>
2022-12-02 01:08:28 -0300

    Resultado após vídeo 5.2

--- galeria/views.py
+++ galeria/views.py
@@ -11,4 +11,11 @@ def imagem(request, foto_id):
     return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
 
 def buscar(request):
-    return render(request, "galeria/buscar.html")
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
+
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+
+    return render(request, "galeria/buscar.html", {"cards": fotografias})
