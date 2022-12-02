16224e3ca88c1cb8982beafd87123234c0c9b5f8
Bruno Divino <brunodivino.dev@gmail.com>
2022-12-02 00:51:13 -0300

    Resultado após vídeo 4.3

--- galeria/models.py
+++ galeria/models.py
@@ -20,4 +20,4 @@ class Fotografia(models.Model):
     data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
 
     def __str__(self):
-        return f"Fotografia [nome={self.nome}]"
+        return self.nome
