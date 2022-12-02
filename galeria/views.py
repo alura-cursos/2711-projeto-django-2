c5ed83326be2eb3d08516b1bdce05badeb905164
Bruno Divino <brunodivino.dev@gmail.com>
2022-12-02 01:00:44 -0300

    Resultado após vídeo 5.1

--- galeria/views.py
+++ galeria/views.py
@@ -9,3 +9,6 @@ def index(request):
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
+
+def buscar(request):
+    return render(request, "galeria/buscar.html")
