f02dc438fb6d9486bb556e85c41001a9b459843e
Bruno Divino <brunodivino.dev@gmail.com>
2022-12-01 21:53:33 -0300

    Resultado após vídeo 1.4

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Fotografia(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.CharField(max_length=150, null=False, blank=False)
+
+    def __str__(self):
+        return f"Fotografia [nome={self.nome}]"
