171a79388b3e6c8665838b18e8e94bff0338cce3
Bruno Divino <brunodivino.dev@gmail.com>
2022-12-02 00:33:33 -0300

    Resultado após vídeo 4.1

--- galeria/models.py
+++ galeria/models.py
@@ -15,7 +15,7 @@ class Fotografia(models.Model):
     legenda = models.CharField(max_length=150, null=False, blank=False)
     categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False)
-    foto = models.CharField(max_length=150, null=False, blank=False)
+    foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
     publicada = models.BooleanField(default=False)
     data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
 
