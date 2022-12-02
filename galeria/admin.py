c12a63f16bbad2c0df656eaee8bee912d71ae7be
Bruno Divino <brunodivino.dev@gmail.com>
2022-12-01 22:38:01 -0300

    Resultado após vídeo 3.1

--- galeria/admin.py
+++ galeria/admin.py
@@ -6,5 +6,7 @@ class ListandoFotografias(admin.ModelAdmin):
     list_display = ("id", "nome", "legenda")
     list_display_links = ("id","nome")
     search_fields = ("nome",)
+    list_filter = ("categoria",)
+    list_per_page = 10
 
 admin.site.register(Fotografia, ListandoFotografias)
