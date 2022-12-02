aa9253360390675e0a6dbef30ee46442eba87363
Bruno Divino <brunodivino.dev@gmail.com>
2022-12-01 22:43:14 -0300

    Resultado após vídeo 3.2

--- galeria/models.py
+++ galeria/models.py
@@ -14,6 +14,7 @@ class Fotografia(models.Model):
     categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=150, null=False, blank=False)
+    publicada = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Fotografia [nome={self.nome}]"
--- galeria/views.py
+++ galeria/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
 
 def index(request):
-    fotografias = Fotografia.objects.all()
+    fotografias = Fotografia.objects.filter(publicada=True)
     return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def imagem(request, foto_id):
